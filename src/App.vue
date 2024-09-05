0c1709b2414e08380f45164aaba78835a343484c
Cristian Diaz <diazcristian0210@gmail.com>
2024-09-04 21:31:58 -0300

    Limpiamos la estructura del codigo y comenzamos a trabajar en el

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const nombreGrupo = "Dynamic Devs"
+</script>
+
+<template>
+  <header>
+    <h1>{{ nombreGrupo }}</h1>
+    <p>Bienvenido a Vue 3</p>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
